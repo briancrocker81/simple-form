9e04b8ccc574af0f25579cb518a79c3b9ec846ad
Brian Crocker <briancrocker81@gmail.com>
2017-05-15 19:53:21 +0100

    basic form

--- send.php
+++ send.php
@@ -2,7 +2,7 @@
   if (isset($_POST['hidden'])) {
     error_log('Possible spam bot');
   } else {
-    $to = "brian@briancrocker.co.uk";
+    $to = "your_email@provider.com";
     $from = $_POST['email'];
     $name = $_POST['name'];
     $subject = "Form submission from " . $name;
